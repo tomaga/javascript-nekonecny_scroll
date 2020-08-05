ebfea8abcabbd726e4dda6bbebf4183ee6b00348
unknown <rtfm@seznam.czm>
2020-08-05 11:44:58 +0200

    lekce 21 hotova

--- script.js
+++ script.js
@@ -63,6 +63,22 @@ async function getPhotos(){
     }
 }
 
+//Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+      /*
+        console.log('window.innerHeight:', window.innerHeight);
+        console.log('window.scrollY:', window.scrollY);
+        console.log('window.innerHeight + scrollY:', window.scrollY + window.innerHeight);
+        console.log('document.body.offsetHeight - 1000:', document.body.offsetHeight -1000);
+        console.log('load more');
+      */
+        getPhotos();
+        console.log('load more');
+    }
+});
+
+
 
 //On Load 
 getPhotos();
